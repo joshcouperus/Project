026fc06e5a6318aeb15ab9e51c442b1d9cb74de8
joshcouperus <35544095+joshcouperus@users.noreply.github.com>
2018-04-21 18:44:36 -0400

    Add files via upload
    added onclick and made a few css changes.

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+
+
+document.getElementById("minion").style.display = "none";
+
+
+function show(){
+	document.getElementById("minion").style.display = "block";
+	
+}
+
+function hide(){
+	document.getElementById("minion").style.display = "none";
+	
+}
+
+function menu(x) {
+    x.classList.toggle("change");
+}
+
+
