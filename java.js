f468a4cec6357eedef02307efaf2ef60d741d5cd
basiljoseph018 <35543673+basiljoseph018@users.noreply.github.com>
2018-04-24 19:47:26 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+
+document.getElementById("minion").style.display = "none";
+
+
+function show(){
+	document.getElementById("minion").style.display = "block";
+	
+}
+
+function hide(){
+	document.getElementById("minion").style.display = "none";
+	
+}
+
+function menu(x) {
+    x.classList.toggle("change");
+}
